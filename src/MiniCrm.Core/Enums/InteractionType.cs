e1cead9460a18cdf77ca5b8c074529fb73f5b421
Akram Fadel <af8356207@gmail.com>
2026-09-24 18:06:09 +0100

    created Enums

---
+++ src/MiniCrm.Core/Enums/InteractionType.cs
@@ -0,0 +1,8 @@
+namespace MiniCrm.Core.Enums;
+
+public enum InteractionType : byte
+{
+    Call    = 1,
+    Email   = 2,
+    Meeting = 3
+}
---
+++ src/MiniCrm.Core/Enums/ReminderStatus.cs
@@ -0,0 +1,8 @@
+namespace MiniCrm.Core.Enums;
+
+public enum ReminderStatus : byte
+{
+    Pending   = 0,
+    Done      = 1,
+    Cancelled = 2
+}
